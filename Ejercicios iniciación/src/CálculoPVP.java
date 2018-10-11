8a815262291f7428bee8fc4ef2bcd6fdd0ba811a
Yaiza Senra <yaizasengon@gmail.com>
2018-10-11 14:17:46 +0200

    Calcular el IVA de precio

---
+++ Ejercicios iniciación/src/CálculoPVP.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CálculoPVP {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Introduce el precio: ");
+		float precio = in.nextFloat();
+		System.out.println("Tipo de IVA: (0) 7% - (1) 10% - (2) 21%");
+		int tipoiva = in.nextInt();
+		float iva;
+		if(tipoiva == 0)
+			iva = 0.07f;
+		else if (tipoiva == 1)
+			iva = 0.1f;
+		else if (tipoiva == 2) 
+			iva = 0.2f;
+		else
+			System.out.println("El tipo de IVA no es correcto");
+		
+		if (tipoiva >= 0 && tipoiva <-2) {
+			
+		}
+		in.close();
+				
+	}
+
+}
