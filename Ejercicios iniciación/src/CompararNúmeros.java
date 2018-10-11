4e623228c5971907cd2915ddd0342702124a9daa
Yaiza Senra <yaizasengon@gmail.com>
2018-10-11 12:42:41 +0200

    Ejercicio de comparación de números

--- Ejercicios iniciación/src/CompararNúmeros.java
+++ Ejercicios iniciación/src/CompararNúmeros.java
@@ -9,8 +9,8 @@ public class CompararNúmeros {
 		int numero2 = r.nextInt(1000);
 		System.out.println("Número 1: " + numero1);
 		System.out.println("Número 2: " + numero2);
-		if (numero1 > numero2)
-				System.out.println("numero1")
+		//if (numero1 > numero2)
+				//System.out.println("numero1")
 	}
 
 }
