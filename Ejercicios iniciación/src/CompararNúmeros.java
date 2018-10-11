1dc0ae8e7457a15f7906b337ef5a26ffcb3697eb
Yaiza Senra <yaizasengon@gmail.com>
2018-10-11 12:47:11 +0200

    .

--- Ejercicios iniciación/src/CompararNúmeros.java
+++ Ejercicios iniciación/src/CompararNúmeros.java
@@ -11,6 +11,8 @@ public class CompararNúmeros {
 		System.out.println("Número 2: " + numero2);
 		if (numero1 > numero2)
 			System.out.println(numero1 + "es mayor que" + numero2);
+		else
+			System.out.println(numero2 + "es mayor que" + numero1);
 		
 	}
 
